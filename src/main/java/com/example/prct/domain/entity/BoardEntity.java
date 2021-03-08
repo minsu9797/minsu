88ae18994acabc3eea4a799c7234bd8c834278eb
minsu9797 <jc041515@naver.com>
2021-03-08 13:47:39 +0900

    minsu

---
+++ src/main/java/com/example/prct/domain/entity/BoardEntity.java
@@ -0,0 +1,42 @@
+package com.example.prct.domain.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table()
+public class BoardEntity extends TimeEntity {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @Column(length = 10, nullable = false)
+    private String writer;
+
+    @Column(length = 225, nullable = false )
+    private String content;
+
+    @Builder
+    public BoardEntity(Long id, String title, String writer, String content) {
+        this.id = id;
+        this.title = title;
+        this.writer = writer;
+        this.content = content;
+    }
+
+}
+
---
+++ src/main/java/com/example/prct/domain/repository/BoardRepository.java
@@ -0,0 +1,10 @@
+package com.example.prct.domain.repository;
+
+import com.example.prct.domain.entity.BoardEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+    List<BoardEntity> findByTitleContaining(String keyword);
+}
